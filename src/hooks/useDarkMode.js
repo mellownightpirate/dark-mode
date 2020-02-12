07d7a43e4dabb5f9b1bbc30940c26c59f9c264e7
Amin Hasan <amin@Amins-MacBook-Air.local>
2020-02-12 00:55:15 +0000

    built a second custom hook that sets dark-mode class on the body element

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,24 @@
+import useLocalStorage from './useLocalStorage';
+import {useEffect} from 'react';
+
+const useDarkMode = (key, initialValue) => {
+    const [darkModeOn, setDarkModeOn] = useLocalStorage(key, initialValue);
+
+    const toggleDarkMode = e => {
+        e.preventDefault();
+
+        return darkModeEnabled ? setDarkModeOn(false) : setDarkModeOn(true);
+    };
+
+    useEffect(() => {
+        if (darkModeOn === true) {
+            document.body.classList.add("dark-mode");
+        } else {
+            document.body.classList.remove("dark-mode");
+        }
+    }, [darkModeOn]);
+
+  return [darkModeOn, setDarkModeOn, toggleDarkMode];
+};
+
+export default useDarkMode;
